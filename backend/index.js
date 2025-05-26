91aa0dcf827fd5626aefed06ac6524c5f7ab57ac
ASM <shidiqmisbahudin12@gmail.com>
2025-05-26 11:55:49 +0700

    bimillah

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
+import express from "express";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import express from "express"
-import cors from "cors"
-import UserRoute from "./routes/UserRoute.js"
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
 dotenv.config();
 
 const app = express();
@@ -12,4 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server up and running on port ${port}`);
+});
